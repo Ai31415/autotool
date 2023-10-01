9a24f7ec92cc67b63ad01513ec6e741b9e70c81a
Ai31415 <146651094+Ai31415@users.noreply.github.com>
2023-10-02 00:58:06 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-        function removeMatchingText() {
-            var temp = 1
-            if(temp == 1){}
-            var inputString = document.getElementById("inputString").value;
-            var re = /^[0-9]* /gm; // 複数行対応の正規表現
-            var resultString = inputString.replace(re, "");
-            document.getElementById("outputString").value = resultString;
-        }
